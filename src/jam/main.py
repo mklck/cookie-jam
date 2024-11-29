5e02c18c68d8e196e913eacaccde3f7eae4d3a00
Szyszak333 <szyszak333@gmail.com>
2024-11-29 20:46:55 +0100

    added Entity class

--- src/jam/main.py
+++ src/jam/main.py
@@ -1,2 +1,13 @@
 def main():
 	print("Hello World!")
+	## moje zmiany:
+	from map_old import Map
+	from Entity import Entity
+	n=70
+	m=70
+	testmap=Map(n,m)
+	for i in range (n):
+		for j in range (m):
+			if(i%2==0 and j%2==0):
+				testmap.set_tile_colour(i,j,"#b123b7");
+	Roman = Entity(15, 15,)
